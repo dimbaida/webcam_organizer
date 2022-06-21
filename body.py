824731510374736e0d2f2c4982f0c1af96276e13
dimbaida [dim-mac] <dimbaida@gmail.com>
2022-06-21 10:36:42 +0200

    final

--- body.py
+++ body.py
@@ -1,47 +1,28 @@
-import os
+from pathlib import Path
 from PIL import Image
 
+src = Path("/Users/dim/Documents/Resident/Resident/webcam/")
+dst = Path("/Users/dim/Documents/Resident/Resident/webcam_processed/")
 
-def process_images(source_folder, destination_folder):
+if not dst.is_dir():
+    dst.mkdir(parents=True)
 
-    if not os.path.exists(destination_folder):
-        os.mkdir(destination_folder)
+files = sorted(src.rglob("*.jpg"))
 
-    counter = 0
-    prev_filesize = 0
-    corrupted_files = []
-    doubled_files = []
+counter = 0
+for i in range(0, len(files)):
+    f = files[i]
+    f_new = dst / (str(counter).zfill(6) + '.jpg')
 
-    for root, dirs, files in os.walk(source_folder):
-        for f in files:
-            old = os.path.join(root, f)
-            print('Now processing: ' + os.path.basename(f), sep="")
-            ext = os.path.splitext(f)[1]
-            new = os.path.join(destination_folder, 'webcam_' + str(counter).zfill(6) + ext)
-
-            curr_filesize = os.path.getsize(old)
-
-            if curr_filesize != prev_filesize:
-                if os.path.exists(new):
-                    os.remove(new)
-                try:
-                    im = Image.open(old)
-                    im.save(new)
-                except IOError:
-                    corrupted_files.append(old)
-                    print('>>>>>>> Corrupted')
-                    continue
-                counter += 1
-                prev_filesize = curr_filesize
-                print(' >> Ok')
-            else:
-                doubled_files.append(old)
-                print('>>>>>>> Doubled')
-
-    print('Corrupted:')
-    for f in corrupted_files:
-        print(os.path.basename(f))
-
-    print('Doubled:')
-    for f in doubled_files:
-        print(os.path.basename(f))
+    if i > 0:
+        f_prev = files[i - 1]
+        if f.stat().st_size == f_prev.stat().st_size:
+            print(f.stem, f_new.stem, 'skipped [duplicated]', sep=' ---> ')
+            continue
+    try:
+        img = Image.open(f)
+        img.save(f_new)
+        print(f.stem, f_new.stem, 'saved', sep=' ---> ')
+        counter += 1
+    except OSError or IOError:
+        print(f.stem, f_new.stem, 'skipped [bad file]', sep=' ---> ')
